47a4e767cbb07d410f6f8473a9993194bda74764
Rahul <raht009@gmail.com>
2024-09-08 10:24:07 +0530

    donechanges

---
+++ jsx/src/App.js
@@ -0,0 +1,11 @@
+const App = function () {
+  //   let inputType = "number";
+  //   let minValue = 5;
+
+  return <h1>Hi there </h1>;
+};
+const akash = "got package of 45 what you have?";
+
+export { akash };
+
+export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,22 +2,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import App, { akash } from "./App";
 //get a reference to the DIV with ID Root
 const el = document.getElementById("root");
 
 //tell react to take control of that element
 
 const root = ReactDOM.createRoot(el);
-// we will create a component
-
-const App = function () {
-  let inputType = "number";
-  let minValue = 5;
-
-  return <textarea autoFocus spellCheck />;
-};
-
+console.log(akash);
 // show the component on the screen
 
 root.render(<App />);
