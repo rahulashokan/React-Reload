6728dfb78e66c2fcc18335207663d6d5939bf235
Rahul <raht009@gmail.com>
2024-09-19 20:54:14 +0530

    props

--- cortana/src/App.js
+++ cortana/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./profileCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Personal Digital Assistants</div>
+      <ProfileCard title="Alexa " handle="@Alexa99" />
+      <ProfileCard title="Cortana " handle="@Cortana99" />
+      <ProfileCard title="Siri " handle="@Siri01" />
     </div>
   );
 }
---
+++ cortana/src/profileCard.js
@@ -0,0 +1,12 @@
+function ProfileCard(props) {
+  return (
+    <div>
+      <div>Title is {props.title}</div>
+
+      <div>Handle will be {props.handle}</div>
+
+      <br></br>
+    </div>
+  );
+}
+export default ProfileCard;
