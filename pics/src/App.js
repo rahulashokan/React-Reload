0d361cc6d5425eeb604eae8531c240e0781a0c06
Rahul <raht009@gmail.com>
2024-10-04 21:25:58 +0530

    changesdone"

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,6 +1,15 @@
-import API from "./API";
+import searchImages from "./API";
+import SearchBar from "./components/SearchBar";
 function App() {
-  return <div>App</div>;
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+    console.log(result);
+  };
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
--- pics/src/index.js
+++ pics/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
+import searchImages from "./API";
 
+searchImages();
 const el = document.getElementById("root");
 
 const root = ReactDom.createRoot(el);
