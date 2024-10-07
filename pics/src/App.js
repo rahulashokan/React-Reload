7a9f3d0ab420007074021f17b32a50ffb1dda801
Rahul <raht009@gmail.com>
2024-10-07 21:54:28 +0530

    imagesprops

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import searchImages from "./API";
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
+
 function App() {
+  const [image, setImage] = useState([]);
+
   const handleSubmit = async (term) => {
     const result = await searchImages(term);
-    console.log(result);
+    setImage(result);
   };
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList image={image} />
     </div>
   );
 }
